f60bb9f886ecbc989cfe8139626a03b0cae43414
yishai <yishai5555@gmail.com>
2021-02-28 16:21:14 +0200

    02-load-dynamic-proto

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const protoLoader = require('@grpc/prot-loader')
+const grpc = require('grpc')
+
+// grpc service definition for greet
+
+const greetProtoPath = path.join(__dirname, '..', 'protos', 'greet.proto')
+const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defualts: true,
+    oneofs: true
+})
+
+const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet
